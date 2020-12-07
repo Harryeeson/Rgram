a44a2102e22d568cf97d1311581003bbfc22d32f
Jihad <jihadshalabi18@gmail.com>
2020-12-06 19:43:09 -0800

    rename cs166 folder to cs179

---
+++ cs179/Project/Implementation/sql/main.sql
@@ -0,0 +1,79 @@
+﻿CREATE TABLE Users (
+    uid INTEGER NOT NULL,
+    fname VARCHAR(32),
+    lname VARCHAR(32),
+    num_follows INTEGER,
+    num_followers INTEGER,
+    PRIMARY KEY(uid)
+);
+
+CREATE TABLE Photo (
+    pid INTEGER NOT NULL,
+    title CHAR(32) NOT NULL,
+    likes INTEGER NOT NULL,
+    dislikes INTEGER NOT NULL, 
+    date CHAR(32) NOT NULL,
+    publishing_user_id INTEGER NOT NULL, 
+    PRIMARY KEY (pid),
+FOREIGN KEY (uid) REFERENCES Users(uid)
+);
+
+CREATE TABLE Has (
+    userid  INTEGER NOT NULL,
+    photoid  INTEGER NOT NULL,
+    PRIMARY KEY(userid, photoid),
+    FOREIGN KEY (userid) REFERENCES Users (uid),
+    FOREIGN KEY (photoid) REFERENCES Photo(pid)
+);
+
+CREATE TABLE Follows(
+    userid1  INTEGER NOT NULL,
+    userid2  INTEGER NOT NULL,
+    FOREIGN KEY (userid1) REFERENCES Users (uid),
+    FOREIGN KEY (userid2) REFERENCES Users (uid),
+    PRIMARY KEY(userid1,userid2)
+);
+
+
+
+
+
+**********************modified version of the code above****************
+CREATE TABLE Users (
+    uid INTEGER NOT NULL,
+    fname VARCHAR(32),
+    lname VARCHAR(32),
+    num_follows INTEGER,
+    num_followers INTEGER,
+    PRIMARY KEY(uid)
+);
+
+
+CREATE TABLE Photo (
+    pid INTEGER NOT NULL,
+    uid INTEGER NOT NULL,
+    title CHAR(32) NOT NULL,
+    likes INTEGER NOT NULL,
+    dislikes INTEGER NOT NULL, 
+    date CHAR(32) NOT NULL,
+    publishing_user CHAR(32) NOT NULL, 
+    PRIMARY KEY (pid),    
+    FOREIGN KEY (uid) REFERENCES Users(uid)
+);
+
+CREATE TABLE Has (
+    userid  INTEGER NOT NULL,
+    photoid  INTEGER NOT NULL,
+    PRIMARY KEY(userid, photoid),
+    FOREIGN KEY (userid) REFERENCES Users (uid),
+    FOREIGN KEY (photoid) REFERENCES Photo(pid)
+);
+
+CREATE TABLE Follows(
+    userid1  INTEGER NOT NULL,
+    userid2  INTEGER NOT NULL,
+    FOREIGN KEY (userid1) REFERENCES Users (uid),
+    FOREIGN KEY (userid2) REFERENCES Users (uid),
+    PRIMARY KEY(userid1,userid2)
+);
+
